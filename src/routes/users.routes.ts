fce60cb7578df17ee3fb118f1e2743e9b7f0786c
Elson Pinheiro Silva <engelp@gmail.com>
2020-10-05 00:38:19 -0300

    implement  upload de img

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,16 @@
 // Rota: Receber uma requisição, chamar outro arquivo, devolver uma resposta
 
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 // SoC: Separations of Cocerns (Separação de preoucupações)
 // DTO: Data Transfer Object
@@ -28,5 +34,28 @@ usersRouter.post('/', async (req, res) => {
     return res.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req, res) => {
+    console.log(req.file);
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: req.user.id,
+        avatarFilename: req.file.filename,
+      });
+
+      delete user.password;
+
+      return res.json(user);
+    } catch (err) {
+      return res.status(400).json({ error: err.message });
+    }
+  },
+);
 
 export default usersRouter;
